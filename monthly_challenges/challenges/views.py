7ccd0425405ccca55cad98dcc588c2179a7ce5b4
Praful Maka <57310445+prafulmaka@users.noreply.github.com>
2022-05-13 01:59:00 -0700

    added urls
    added additional urls

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("January: ")
+
+def february(request):
+    return HttpResponse("February: ")
