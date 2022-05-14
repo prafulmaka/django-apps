3b8f7e0f6712f85466432aa31a907ca9d67909ec
Praful Maka <57310445+prafulmaka@users.noreply.github.com>
2022-05-14 12:26:55 -0700

    05142022
    added list to index page

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:month>", views.monthly_challenge),
-    path("<int:month>", views.monthly_challenge_by_number)
+    path("", views.index),
+    path("<int:month>", views.monthly_challenge_by_number, name="month-challenge"),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
